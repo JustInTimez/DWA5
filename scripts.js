29aab7edc76645bc5deaedbbf69313e4193d63c1
JustInTimez <just.oost@gmail.com>
2023-04-24 14:45:32 +0200

    Updates code

--- scripts.js
+++ scripts.js
@@ -5,24 +5,29 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  //   Scenario: Validation when values are missing
+
+  //   Scenario 2: Validation when values are missing
   if (dividend === "" || divider === "") {
     result.innerText =
       "Division not performed. Both values are required in inputs. Try again";
   }
-  //   Scenario: Providing anything that is not a number should crash the program
-  if (isNaN(dividend) || isNaN(divider)) {
-    console.error(new Error("Something critical went wrong. Please reload the page."));
+
+  //   Scenario 4: Providing anything that is not a number should crash the program
+  else if (isNaN(dividend) || isNaN(divider)) {
+    console.error(
+      new Error("Something critical went wrong. Please reload the page."));
     document.body.innerHTML =
       "<h1>Something critical went wrong. Please reload the page.</h1>";
   }
-  //   Scenario: An invalid division should log an error in the console
-  if (dividend < 0 || divider < 0) {
+
+  //   Scenario 3: An invalid division should log an error in the console
+  else if (dividend < 0 || divider < 0) {
     result.innerText =
       "Division not performed. Invalid number provided. Try again";
     console.trace();
+
+    //   Scenario 1: Dividing numbers result in a decimal number
   } else {
-    //   Scenario: Dividing numbers result in a decimal number
     result.innerText = Math.floor(dividend / divider);
   }
 });
