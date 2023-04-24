bb3db7fd634fbd911ae855a020c66521e84bcabf
JustInTimez <just.oost@gmail.com>
2023-04-24 13:23:44 +0200

    Adds fix for scenario
    Scenario: Dividing numbers result in a decimal number fix

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+//   Scenario: Dividing numbers result in a decimal number
+  result.innerText = Math.floor(dividend / divider);
+});
